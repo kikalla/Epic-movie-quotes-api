63aa02be7b025a904a43ce6b92a7ee549b35d1a6
kikadzeRedberry <giorgikikadze@redberry.ge>
2022-12-09 00:29:00 +0400

    feat: create email add,remove functional, emails migration

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\NewEmail;
 use App\Mail\SignupEmail;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class MailController extends Controller
 {
@@ -23,15 +26,31 @@ class MailController extends Controller
 
 		if ($request->route('token') === $check_token->token)
 		{
-			User::where('email', $check_token->email)->update([
-				'email_verified'=> 'verified',
-			]);
+			$user = User::where('email', $check_token->email);
+			if ($user->first())
+			{
+				$user->update([
+					'email_verified' => 'verified',
+				]);
 
-			DB::table('email_verifications')->where([
-				'email'=> $check_token->email,
-			])->delete();
+				DB::table('email_verifications')->where([
+					'email'=> $check_token->email,
+				])->delete();
 
-			return redirect(config('movie-quotes.app-front-url') . '/verified');
+				return redirect(config('movie-quotes.app-front-url') . '/verified');
+			}
+			else
+			{
+				DB::table('users_emails')->where([
+					'email'=> $check_token->email,
+				])->update(['email_verified' => 'verified']);
+
+				DB::table('email_verifications')->where([
+					'email'=> $check_token->email,
+				])->delete();
+
+				return redirect(config('movie-quotes.app-front-url') . '/profile');
+			}
 		}
 		else
 		{
@@ -47,5 +66,121 @@ class MailController extends Controller
 		];
 
 		Mail::to($email)->send(new SignupEmail($data));
+	}
+
+	public static function addEmail(Request $request)
+	{
+		$user = User::where('id', jwtUser()->id)->first();
+
+		if ($user->email_verified === 'verified-google')
+		{
+			return 'Cant add email with google registrations';
+		}
+
+		if (DB::table('users_emails')->where('email', $request->email)->first())
+		{
+			return response('Already exists', 403);
+		}
+
+		DB::table('users_emails')->insert([
+			'user_id' => jwtUser()->id,
+			'email'   => $request->email,
+		]);
+
+		$token = Str::random(64);
+		DB::table('email_verifications')->updateOrInsert(
+			['email'     => $request->email],
+			[
+				'token'     => $token,
+				'created_at'=> Carbon::now(),
+			]
+		);
+
+		$data = [
+			'username'	=> jwtUser()->username,
+			'token'   	=> $token,
+		];
+
+		Mail::to(jwtUser()->email)->send(new NewEmail($data));
+
+		return response('Email created', 200);
+	}
+
+	public function deleteEmail(Request $request)
+	{
+		$email = DB::table('users_emails')->where('email', $request->email)->first();
+
+		if (!$email)
+		{
+			return 'Email not exists or its primary';
+		}
+		if (jwtUser()->id == $email->user_id)
+		{
+			DB::table('users_emails')->where('email', $request->email)->delete();
+			return 'Email deleted';
+		}
+		else
+		{
+			return 'Wrong user';
+		}
+	}
+
+	public function makePrimary(Request $request)
+	{
+		$email = DB::table('users_emails')->where('email', $request->email)->first();
+
+		if (!$email)
+		{
+			return response('Email not exists', 404);
+		}
+		if (jwtUser()->id != $email->user_id)
+		{
+			return response('Wrong user', 401);
+		}
+		if ($email->email_verified === 'not-verified')
+		{
+			return response('Verify first', 401);
+		}
+
+		$user = User::where('id', jwtUser()->id)->first();
+		$oldEmail = $user->email;
+
+		DB::table('users_emails')->where('email', $email->email)->delete();
+
+		$user->update(['email' => $email->email]);
+
+		DB::table('users_emails')->insert([
+			'user_id'          => $user->id,
+			'email'            => $oldEmail,
+			'email_verified'   => 'verified',
+		]);
+
+		return 'Success';
+	}
+
+	public function sendEmails()
+	{
+		$emails = [];
+		$verifieds = [];
+
+		$notPrimaryEmails = DB::table('users_emails')->where('user_id', jwtUser()->id)->get();
+
+		foreach ($notPrimaryEmails as $email)
+		{
+			$emails[] = $email->email;
+			if ($email->email_verified === 'verified')
+			{
+				$verifieds[] = true;
+			}
+			else
+			{
+				$verifieds[] = false;
+			}
+		}
+
+		$emails[] = jwtUser()->email;
+		$verifieds[] = true;
+
+		return [$emails, $verifieds];
 	}
 }
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\GoogleController;
 use App\Http\Controllers\MailController;
 use App\Http\Controllers\MovieController;
-use App\Http\ControlleApp\Http\Controllers\ResetPasswordController;
+use App\Http\Controllers\ResetPasswordController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\QuoteController;
 use Illuminate\Support\Facades\Route;
@@ -38,6 +38,10 @@ Route::post('/forgot/password', [ResetPasswordController::class, 'forgotPassword
 Route::post('/reset/password', [ResetPasswordController::class, 'resetPassword'])->name('reset.password');
 
 Route::get('/email/verify/{token}', [MailController::class, 'verifyEmail'])->name('verification.verify');
+Route::post('/add-email', [MailController::class, 'addEmail']);
+Route::post('/delete-email', [MailController::class, 'deleteEmail']);
+Route::post('/make-primary', [MailController::class, 'makePrimary']);
+Route::post('/get-emails', [MailController::class, 'sendEmails']);
 
 Route::controller(MovieController::class)->group(function () {
 	Route::post('/movies/add-movie', 'store');
